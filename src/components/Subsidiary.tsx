b2eea3daa13748e5fe82ceb42ea5ff3ca12502b1
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-04-02 11:50:15 +0200

    Home transition

--- src/components/Subsidiary.tsx
+++ src/components/Subsidiary.tsx
@@ -12,19 +12,21 @@ export default function Subsidiary({
     <section
       className={
         additionnalClassName
-          ? className + " " + additionnalClassName
-          : className
+          ? className + " " + additionnalClassName + " group"
+          : className + " group"
       }
       id={index}
     >
       <div className="logo_subsection flex flex-col items-center text-2xl">
         <img src={src} alt={alt} />
-        <div>
+        <div className="flex flex-col gap-y-10">
           <span className="block max-w-187.5 text-center break-normal">
             {catchphrase}
           </span>
-          <div className="button flex flex-col items-center ">
-            <button>En savoir plus</button>
+          <div className="button flex flex-col items-center overflow-hidden">
+            <button className="border border-solid border-white font-bold text-base rounded-sm uppercase p-2.5 w-fit translate-y-full group-hover:translate-y-0 transition-transform duration-300 cursor-pointer">
+              En savoir plus
+            </button>
           </div>
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 
 function Home() {
   const subsidiariesClassName: string =
-    "flex w-full justify-center bg-red p-28.75 h-fit text-white";
+    "flex w-full justify-center bg-red p-28.75 h-fit";
   const subsidiaries: SubsidiaryType[] = [
     {
       src: "/images/Visuance.png",
@@ -21,21 +21,21 @@ function Home() {
       alt: "Pulse X Image",
       catchphrase: "Social Agency - Media and Community Management",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-black",
+      additionnalClassName: "bg-black text-white",
     },
     {
       src: "/images/Pulse X Agency.png",
       alt: "Pulse X Agency Image",
       catchphrase: "Influence Agency and Content Creator",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-pulse-x-agency-pink",
+      additionnalClassName: "bg-pulse-x-agency-pink text-white",
     },
     {
       src: "/images/Snackly.png",
       alt: "Snackly Image",
       catchphrase: "Snack Content Agency",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-snackly-orange",
+      additionnalClassName: "bg-snackly-orange text-white",
     },
     {
       src: "/images/Seofy.png",
@@ -43,35 +43,35 @@ function Home() {
       catchphrase:
         " Search Engines (SEO) Agency of AI and Media Buying SEA,\nSocial Ads and Programmatic",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-seofy-green",
+      additionnalClassName: "bg-seofy-green text-white",
     },
     {
       src: "/images/Drox 360.png",
       alt: "Drox 360 Image",
       catchphrase: "Creative Energy Agency - Innovative",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-black",
+      additionnalClassName: "bg-black text-white",
     },
     {
       src: "/images/Sky To Be Media.png",
       alt: "Sky To Be Media Image",
       catchphrase: "Media Production Society",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-black",
+      additionnalClassName: "bg-black text-white",
     },
     {
       src: "/images/Play To Sky Productions.png",
       alt: "Play To Sky Productions Image",
       catchphrase: "Movies Production Society",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-play-to-sky-productions-gray",
+      additionnalClassName: "bg-play-to-sky-productions-gray text-white",
     },
     {
       src: "/images/CYBERSKY.png",
       alt: "CYBERSKY Image",
       catchphrase: "IT Solutions and solutions for corporations",
       className: subsidiariesClassName,
-      additionnalClassName: "bg-black",
+      additionnalClassName: "bg-black text-white",
     },
   ];
   return (
